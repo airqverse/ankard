7e57c683156fcfa2b96032b3f2df60265eb83401
airq <airq1106@gmail.com>
2025-05-19 22:53:17 +0900

    Learned to use Dynamic path segments & Captured Values

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.january),
-    path("february", views.february),
-    path("march", views.march)
+    # path("january", views.january),
+    # path("february", views.february),
+    # path("march", views.march),
+
+    # The word "month" in "<month>" has to be correspondent to
+    # the argument name in views.monthly_challenge function.
+    path("<month>", views.monthly_challenge)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def january(request):
-    return HttpResponse("Eat no meat for the entire month!")
+# def january(request):
+#     return HttpResponse("Eat no meat for the entire month!")
 
-def february(request):
-    return HttpResponse("Walk for at least 20 minutes everyday!")
+# def february(request):
+#     return HttpResponse("Walk for at least 20 minutes everyday!")
 
-def march(request):
-    return HttpResponse("Learn Django for at least 20 minutes everyday!")
+# def march(request):
+#     return HttpResponse("Learn Django for at least 20 minutes everyday!")
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Jan: Eat no meat for the entire month!"
+    elif month == "february":
+        challenge_text = "Feb: Walk for at least 20 minutes everyday!"
+    elif month == "march":
+        challenge_text = "Mar: Learn Django for at least 20 minutes everyday!"
+    else:
+        return HttpResponseNotFound("Month: " + month + " is not supported.")
+
+    return HttpResponse(challenge_text)
