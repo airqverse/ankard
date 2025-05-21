8ebf8f6809c0c4a2f307099ff186376b6d35cd01
airq <airq1106@gmail.com>
2025-05-21 10:24:07 +0900

    Learned to use django.urls.redirect and "name of path"

--- challenges/urls.py
+++ challenges/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
 
     # The word "month" in "<month>" has to be correspondent to
     # the argument name in views.monthly_challenge function.
-    path("<int:month>", views.monthly_challenge_by_number),
-    path("<str:month>", views.monthly_challenge)
+    path("<int:month>", views.monthly_challenge_by_number, name="month-challenge-by-number"),
+    path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
