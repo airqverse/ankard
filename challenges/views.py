6aad2692979966c9f3cd2b06bf5c896e96d9eaa5
airq <airq1106@gmail.com>
2025-05-23 10:40:05 +0900

    Completed exercise and made some minor changes

--- challenges/views.py
+++ challenges/views.py
@@ -4,21 +4,19 @@ from django.urls import reverse
 
 # Create your views here.
 
-page_title = "Monthly Challenge"
-
 monthly_challenges = {
-    "january": "Here is January's challenge!",
-    "february": "Here is February's challenge!",
-    "march": "Here is March's challenge!",
-    "april": "Here is April's challenge!",
-    "may": "Here is May's challenge!",
-    "june": "Here is June's challenge!",
-    "july": "Here is July's challenge!",
-    "august": "Here is August's challenge!",
-    "september": "Here is September's challenge!",
-    "october": "Here is October's challenge!",
-    "november": "Here is November's challenge!",
-    "december": "Here is December's challenge!",
+    "january": "Learn Bubble Sort!",
+    "february": "Learn Insertion Sort!",
+    "march": "Learn Selection Sort!",
+    "april": "Learn Binary Search!",
+    "may": "Learn Quick Sort!",
+    "june": "Learn Merge Sort!",
+    "july": "Learn DFS(Depth First Search)!",
+    "august": "Learn BFS(Breadth First Search)!",
+    "september": "Learn Django!",
+    "october": "Learn Data Structure!",
+    "november": "Learn TCP/IP!",
+    "december": "Learn Linux!",
 }
 
 def index(request):
@@ -48,12 +46,10 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        monthly_title = month.capitalize() + "'s Challenge"
         # render() acts as "render_to_string() + HttpResponse()"
         return render(request, "challenges/challenge.html", {
             # Send data to frontend as "context"
-            "page_title": page_title,
-            "monthly_title": monthly_title,
+            "month_name": month.capitalize(),
             "text": challenge_text,
         })
     except:
